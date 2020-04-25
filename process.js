a80e771f1db70612cc24c95a6281cb3690a12137
Matthew Kilpatrick <16194494+Matthew-Kilpatrick@users.noreply.github.com>
2020-04-25 21:46:08 +0100

    Added detection for when people start speaking

--- process.js
+++ process.js
@@ -2,70 +2,88 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const speech = require('@google-cloud/speech');
 const fs = require('fs');
+const { Readable } = require('stream');
+
+var channel;
+var connection;
+
+const SILENCE_FRAME = Buffer.from([0xf8, 0xff, 0xfe]);
+
+class SingleSilence extends Readable {
+  _read() {
+    this.push(SILENCE_FRAME);
+    this.push(null);
+  }
+}
 
 client.once('ready', () => {
-        console.log('Ready!');
+  console.log('Ready!');
 });
 
-client.login('NzAzNjUwOTM2NjA5ODMzMDAx.XqRsjA.-jSQD1XXXyAECNMl7uqTnIGC6pY');
+client.login('NzAzNjUwOTM2NjA5ODMzMDAx.XqSebQ.cvnU7ULTOrS9d1P_rpGAQyndF2c');
 
+client.on("message", async (msg) => {
+    if (msg.content.startsWith("/moderate")) {
+        if (msg.member.voice.channel) {
+            connection = await msg.member.voice.channel.join();
+            connection.play(new SingleSilence(), { type: 'opus' });
+            channel = msg.channel;
+        }
+    }
+});
 
-client.on('message', async message => {
-  // Join the same voice channel of the author of the message
-  if (message.member.voice.channel) {
-    console.log('listening');
-    const fileName = 'user_audio_' + message.member.id + '_' + Date.now();
-    console.log(fileName);
-    const connection = await message.member.voice.channel.join();
-    // Create a ReadableStream of s16le PCM audio
-    const audio = connection.receiver.createStream(message.member.id, { mode: 'pcm' });
-    var ws = fs.createWriteStream(fileName);
-    await audio.pipe(ws);
-    ws.on('finish', function() {
-      var SoxCommand = require('sox-audio');
-      var command = SoxCommand();
-      command.input(fileName)
-        .inputSampleRate(48000)
-        .inputEncoding('signed')
-        .inputBits(16)
-        .inputChannels(2)
-        .inputFileType('raw');
-      command.output(fileName + '.wav')
-        .outputChannels(1)
-        .outputSampleRate(48000)
-        .outputFileType('wav');
-      command.run();
-      command.on('end', async function() {
-        console.log('detection done');
-        // Google recognition
-        const client = new speech.SpeechClient();
+client.on("guildMemberSpeaking", async (member, speaking) => {
+  console.log('listening');
+  const fileName = 'user_audio_' + member.id + '_' + Date.now();
+  // Create a ReadableStream of s16le PCM audio
+  const audio = connection.receiver.createStream(member.id, { mode: 'pcm' });
+  var ws = fs.createWriteStream(fileName);
+  await audio.pipe(ws);
+  ws.on('finish', function() {
+    var SoxCommand = require('sox-audio');
+    var command = SoxCommand();
+    command.input(fileName)
+      .inputSampleRate(48000)
+      .inputEncoding('signed')
+      .inputBits(16)
+      .inputChannels(2)
+      .inputFileType('raw');
+    command.output(fileName + '.wav')
+      .outputChannels(1)
+      .outputSampleRate(48000)
+      .outputFileType('wav');
+    command.run();
+    command.on('end', async function() {
+      console.log('detection done');
+      // Google recognition
+      const client = new speech.SpeechClient();
 
-        // Reads a local audio file and converts it to base64
-        const file = fs.readFileSync(fileName + '.wav');
-        const audioBytes = file.toString('base64');
+      // Reads a local audio file and converts it to base64
+      const file = fs.readFileSync(fileName + '.wav');
+      const audioBytes = file.toString('base64');
 
-        // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-        const audio = {
-          content: audioBytes,
-        };
-        const config = {
-          encoding: 'WAV',
-          sampleRateHertz: 48000,
-          languageCode: 'en-US',
-        };
-        const request = {
-          audio: audio,
-          config: config,
-        };
+      // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+      const audio = {
+        content: audioBytes,
+      };
+      const config = {
+        encoding: 'WAV',
+        sampleRateHertz: 48000,
+        languageCode: 'en-US',
+      };
+      const request = {
+        audio: audio,
+        config: config,
+      };
 
-        // Detects speech in the audio file
-        const [response] = await client.recognize(request);
-        const transcription = response.results
-          .map(result => result.alternatives[0].transcript)
-          .join('\n');
-        console.log(`Transcription: ${transcription}`);
-        message.channel.send(`I heard someone say ${transcription}`);
-      });
+      // Detects speech in the audio file
+      const [response] = await client.recognize(request);
+      const transcription = response.results
+        .map(result => result.alternatives[0].transcript)
+        .join('\n');
+      console.log(`Transcription: ${transcription}`);
+      channel.send(`I heard someone say ${transcription}`);
     });
-  }
+  });
 });
+
